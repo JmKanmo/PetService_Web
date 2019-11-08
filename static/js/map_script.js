5bdfdb2e71b71b88f5232ae7b6c9d66ae6ee7d98
JmKanmo <nebi25@naver.com>
2019-11-08 14:15:05 +0900

    맵 스크립트 수정(오버레이)

--- static/js/map_script.js
+++ static/js/map_script.js
@@ -6,6 +6,7 @@ mapOption = {
 
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption);
+map.setKeyboardShortcuts(true);
 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
@@ -30,6 +31,7 @@ infowindow = new kakao.maps.InfoWindow({
 // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
 searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
+// 지도를 클릭했을 때 커스텀을 표시하는 클릭핸들러
 var mapClickHandler = function (mouseEvent) {
 searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
     if (status === kakao.maps.services.Status.OK) {
@@ -61,7 +63,6 @@ searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
 
 //커스텀뷰를 닫는함수
 function window_close(){
-    console.log('닫혀야지');
     infowindow.close();
 }
 
@@ -80,7 +81,6 @@ function roadView(x,y){
 
 //로드뷰를 닫는 함수
 function close_click(){
-    console.log('닫기버튼실행되니?');
     document.getElementById('roadview').style.display='none';
 }
 
@@ -96,50 +96,45 @@ kakao.maps.event.addListener(map, 'click', mapClickHandler);
 marker_click = false;
 }
 
+// 마커를 클릭했을 때 오버레이를 표시하는 클릭핸들러 
+var markerClickHandler = function () {
+    if (marker_click) {
+        kakao.maps.event.addListener(map, 'click', mapClickHandler);
+        overlay.setMap(null);
+        marker_click = false;
+    } else {
+        var content = '<div class="wrap">' +
+            '    <div class="info">' +
+            '        <div class="title">' +
+            '            카카오 스페이스닷원' +
+            '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+            '        </div>' +
+            '        <div class="body">' +
+            '            <div class="img">' +
+            '                <img src="http://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+            '           </div>' +
+            '            <div class="desc">' +
+            '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+            '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+            '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+            '            </div>' +
+            '        </div>' +
+            '    </div>' +
+            '</div>';
+    
+        overlay = new kakao.maps.CustomOverlay({
+            content: content,
+            map: map,
+            position: marker.getPosition()
+        });
+        marker_click = true;
+        overlay.setMap(map);
+        kakao.maps.event.removeListener(map, 'click', mapClickHandler);
+    }
+    };
+
 // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-kakao.maps.event.addListener(marker, 'click', function () {
-if (marker_click) {
-    kakao.maps.event.addListener(map, 'click', mapClickHandler);
-    overlay.setMap(null);
-    marker_click = false;
-} else {
-    var content = '<div class="wrap">' +
-        '    <div class="info">' +
-        '        <div class="title">' +
-        '            카카오 스페이스닷원' +
-        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
-        '        </div>' +
-        '        <div class="body">' +
-        '            <div class="img">' +
-        '                <img src="http://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
-        '           </div>' +
-        '            <div class="desc">' +
-        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-        '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
-        '            </div>' +
-        '        </div>' +
-        '    </div>' +
-        '</div>';
-
-    overlay = new kakao.maps.CustomOverlay({
-        content: content,
-        map: map,
-        position: marker.getPosition()
-    });
-    marker_click = true;
-    overlay.setMap(map);
-    kakao.maps.event.removeListener(map, 'click', mapClickHandler);
-}
-});
-
-// 마커에 마우스를 올렸을 때
-kakao.maps.event.addListener(marker,'mouseover',function(){
-});
-
-// 마어케 마우스를 땠을 때
-kakao.maps.event.addListener(marker,'mouseout',function(){
-});
+kakao.maps.event.addListener(marker, 'click', markerClickHandler);
 
 // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
 kakao.maps.event.addListener(map, 'idle', function () {
@@ -194,4 +189,109 @@ map.setLevel(map.getLevel() - 1);
 // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
 function zoomOut() {
 map.setLevel(map.getLevel() + 1);
+}
+
+//임시로 마커생성
+var dog_marker = new Array();
+
+function marker_make(){
+    // 마커를 표시할 위치와 title 객체 배열입니다 
+    var positions = [
+        {
+            content: '<div>카카오</div>', 
+            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+        },
+        {
+            content: '<div>마카오</div>', 
+            latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+        },
+        {
+            content: '<div>코리아텍</div>', 
+            latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+        },
+        {
+            content: '<div>이리와</div>', 
+            latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+        }
+    ];
+
+    for (var i = 0; i < positions.length; i ++) {
+        
+        // 마커 이미지를 생성합니다    
+        var markerImage = new kakao.maps.MarkerImage("../static/images/dog_icon.png",new kakao.maps.Size(30, 45)); 
+        
+        // 마커에 표시할 인포윈도우를 생성합니다 
+        var dog_infowindow = new kakao.maps.InfoWindow({
+            content: positions[i].content // 인포윈도우에 표시할 내용
+        });
+
+        // 마커를 생성합니다
+        dog_marker[i] = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            content : positions[i].content, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지 
+        });
+
+        // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+        // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+        // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+        kakao.maps.event.addListener(dog_marker[i], 'click', dog_marker_OnClick(map,dog_marker[i],dog_infowindow));
+        kakao.maps.event.addListener(dog_marker[i], 'mouseover', makeOverListener(map, dog_marker[i], dog_infowindow));
+        kakao.maps.event.addListener(dog_marker[i], 'mouseout', makeOutListener(dog_infowindow));
+    } 
+}
+
+//반려견마커 클릭시 오버레이띄우는 함수입니다
+function dog_marker_OnClick(map, dog_marker, dog_infowindow) {
+    return function(){
+        if (marker_click) {
+            kakao.maps.event.addListener(map, 'click', mapClickHandler);
+            overlay.setMap(null);
+            marker_click = false;
+        } else {
+            var content = '<div class="wrap">' +
+                '    <div class="info">' +
+                '        <div class="title">' +
+                '            카카오 스페이스닷원' +
+                '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+                '        </div>' +
+                '        <div class="body">' +
+                '            <div class="img">' +
+                '                <img src="http://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+                '           </div>' +
+                '            <div class="desc">' +
+                '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+                '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+                '                <div><a href="http://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+                '            </div>' +
+                '        </div>' +
+                '    </div>' +
+                '</div>';
+
+            overlay = new kakao.maps.CustomOverlay({
+                content: content,
+                map: map,
+                position: dog_marker.getPosition()
+            });
+
+            marker_click = true;
+            overlay.setMap(map);
+            kakao.maps.event.removeListener(map, 'click', mapClickHandler);
+        }
+    };
+    };
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+function makeOverListener(map, marker, infowindow) {
+    return function() {
+        infowindow.open(map, marker);
+    };
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+function makeOutListener(infowindow) {
+    return function() {
+        infowindow.close();
+    };
 }
